daea3aea86464c9399671a6f4311843f6e8abdb7
Hironori <hironori.hyodo@eaglys.co.jp>
2025-03-03 16:31:51 +0900

    環境構築中

---
+++ dev/frontend/app/vite.config.ts
@@ -0,0 +1,16 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    watch: {
+      usePolling: true,
+      interval: 1000, // default 100
+      binaryInterval: 1500, // default 300
+    },
+  },
+})
